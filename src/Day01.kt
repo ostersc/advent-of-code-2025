ba64aebac5374f55da6e426f191fd0b52ad63639
ostersc <ostersc@gmail.com>
2025-12-02 00:23:17 -0500

    day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,63 @@
 fun main() {
+
+    fun turn(dial: Int, direction: Char, amount: Int): Int {
+        return when (direction) {
+            'L' -> if(dial-amount < 0) {dial - amount + 100} else {dial - amount}
+            'R' -> if(dial+amount >= 100) {dial + amount - 100} else{ dial + amount}
+            else -> amount
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial=50
+        var zeros=0
+        for (line in input) {
+            val direction = line.first()
+            val amount = line.substring(1).toInt() % 100
+            val newDial = turn(dial, direction, amount)
+            check(newDial>=0 && newDial<100)
+            //println("From $dial turn $direction $amount to $newDial")
+            dial=newDial
+            if (dial==0) {
+                zeros+=1
+            }
+        }
+        return zeros
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var dial=50
+        var zeros=0
+        for (line in input) {
+            val direction = line.first()
+            val num = line.substring(1).toInt()
+            val amount = num % 100
+            val multiple = kotlin.math.floor(num / 100.0).toInt()
+            val newDial = turn(dial, direction, amount)
+            check(newDial>=0 && newDial<100)
+            //println("From $dial turn $direction $amount to $newDial")
+            if (direction=='L' && (newDial > dial || newDial==0 ) && dial!=0) {
+                zeros+=1
+            }else if (direction=='R' && (newDial < dial || newDial==0) && dial!=0) {
+                zeros+=1
+            }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+            if(multiple>0){
+                zeros+=multiple
+            }
+            dial=newDial
+            //println("Zero count: $zeros")
+        }
+        return zeros
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+
+    check(part1(testInput) == 3)
     part1(input).println()
+
+    check(part2(testInput) == 6)
     part2(input).println()
 }
