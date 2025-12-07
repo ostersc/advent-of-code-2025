a47de79e8b2827ef21f43608454f0ccc23d88f29
ostersc <ostersc@gmail.com>
2025-12-07 13:36:01 -0500

    day 5 done

--- src/Day05.kt
+++ src/Day05.kt
@@ -11,7 +11,29 @@ fun main() {
         return input.drop(split + 1).count { ingrediant -> freshRanges.any { range->ingrediant.toLong() in range } }
     }
 
-    fun part2(input: List<String>): Int =0
+    fun part2(input: List<String>): Long {
+        val freshRanges=input.takeWhile { it.isNotBlank() }.map {
+                line->
+            val (min,max)=line.split("-")
+            min.toLong()..max.toLong()
+        }
+
+        return freshRanges.sortedBy { it.first }.fold(mutableListOf<LongRange>() ){acc, range->
+            if(acc.isEmpty()){
+                acc.add(range)
+            }else{
+                if(acc.last().last < range.first){
+                    acc.add(range)
+                }else if(acc.last().last< range.last){
+                    acc[acc.size-1]=acc.last().first ..  range.last
+                }
+            }
+            acc
+        }.sumOf { it.last-it.first +1 }
+
+
+
+    }
 
     val testInput = readInput("Day05_test")
     val input = readInput("Day05")
@@ -19,6 +41,6 @@ fun main() {
     println("part1(test) = ${part1(testInput)} (expected 3)")
     part1(input).println()
 
-    println("part2(test) = ${part2(testInput)} (expected ?)")
+    println("part2(test) = ${part2(testInput)} (expected 14)")
     part2(input).println()
 }
